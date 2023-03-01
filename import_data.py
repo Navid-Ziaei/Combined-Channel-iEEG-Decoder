470047ab6208b0b084cb70a2ce736d9919f03eac
Navid Ziaei <n2ziaee@gmail.com>
2023-03-01 17:11:10 +0330

    Add required docs to the code

---
+++ import_data.py
@@ -0,0 +1,199 @@
+import warnings
+
+import mne
+import mne_bids
+import numpy as np
+import pandas as pd
+from collections import OrderedDict
+from ieeg_fmri_validation.utils import resample, smooth_signal, zscore
+
+
+class SubjectDataset(object):
+    def __init__(self, input_root, subject, preload=True, **kwargs):
+        self.root = input_root
+        self.subject = subject
+        self.datatype = 'ieeg'
+        self.acquisition = None
+        self.__dict__.update(kwargs)
+        self.task = 'film'
+        self.expected_duration = 390
+
+        if preload:
+            self._set_paths()
+            self._load_data()
+            self._get_bad_electrodes()
+
+    def _set_paths(self):
+        bids_path = mne_bids.BIDSPath(subject=self.subject,
+                                      task=self.task,
+                                      suffix='ieeg',
+                                      extension='vhdr',
+                                      datatype=self.datatype,
+                                      acquisition=self.acquisition,
+                                      root=self.root)
+
+        print(self.task)
+        assert len(bids_path.match()) == 1, 'None or more than one run for task is found'
+
+        self.raw_path = str(bids_path.match()[0])
+        self.run = mne_bids.get_entities_from_fname(bids_path.match()[0])['run']
+        self.session = mne_bids.get_entities_from_fname(bids_path.match()[0])['session']
+
+        bids_path = mne_bids.BIDSPath(subject=self.subject,
+                                      task=self.task,
+                                      session=self.session,
+                                      suffix='channels', run=self.run,
+                                      extension='tsv',
+                                      datatype=self.datatype,
+                                      acquisition=self.acquisition,
+                                      root=self.root)
+        self.channels_path = str(bids_path)
+
+        bids_path = mne_bids.BIDSPath(subject=self.subject,
+                                      session=self.session, suffix='electrodes',
+                                      extension='tsv',
+                                      datatype=self.datatype,
+                                      acquisition=self.acquisition,
+                                      root=self.root)
+        self.electrodes_path = str(bids_path)
+
+        bids_path = mne_bids.BIDSPath(subject=self.subject,
+                                      suffix='T1w',
+                                      extension='.nii.gz',
+                                      root=self.root)
+        self.anat_path = str(bids_path.match()[0])
+        self.anat_session = mne_bids.get_entities_from_fname(bids_path.match()[0])['session']
+        print('Picking up BIDS files done')
+
+    def _load_data(self):
+        self.raw = mne.io.read_raw_brainvision(self.raw_path,
+                                               eog=(['EOG']),
+                                               misc=(['OTHER', 'ECG', 'EMG']),
+                                               scale=1.0,
+                                               preload=False,
+                                               verbose=True)
+        self.channels = pd.read_csv(self.channels_path, sep='\t', header=0, index_col=None)
+        self.electrodes = pd.read_csv(self.electrodes_path, sep='\t', header=0, index_col=None)
+        self.raw.set_channel_types({ch_name: str(x).lower()
+        if str(x).lower() in ['ecog', 'seeg', 'eeg'] else 'misc'
+                                    for ch_name, x in zip(self.raw.ch_names, self.channels['type'].values)})
+        self.other_channels = self.channels['name'][~self.channels['type'].isin(['ECOG', 'SEEG'])].tolist()
+        self.raw.drop_channels(self.other_channels)
+        print('Loading BIDS data done')
+
+    def _get_bad_electrodes(self):
+        self.bad_electrodes = self.channels['name'][
+            (self.channels['type'].isin(['ECOG', 'SEEG'])) & (self.channels['status'] == 'bad')].tolist()
+        self.all_electrodes = self.channels['name'][(self.channels['type'].isin(['ECOG', 'SEEG']))].tolist()
+        print('Getting bad electrodes done')
+
+    def preprocess(self):
+        raw_dbe = self._discard_bad_electrodes()
+        raw_nf = self._notch_filter()
+        raw_car = self._common_average_reference()
+        return raw_car
+
+    def _discard_bad_electrodes(self):
+        if self.raw is not None:
+            [self.bad_electrodes.remove(i) for i in self.bad_electrodes if i not in self.raw.ch_names]
+            self.raw.info['bads'].extend([ch for ch in self.bad_electrodes])
+            print('Bad channels indicated: ' + str(self.bad_electrodes))
+            print('Dropped ' + str(self.raw.info['bads']) + 'channels')
+            self.raw.drop_channels(self.raw.info['bads'])
+            self.raw.load_data()
+            print('Remaining channels ' + str(self.raw.ch_names))
+            return self.raw
+
+    def _notch_filter(self):
+        if self.raw is not None:
+            if np.any(np.isnan(self.raw._data)):
+                # self.raw._data[np.isnan(self.raw._data)]=0 # bad hack for nan values
+                warnings.warn('There are NaNs in the data, replacing with 0')
+            self.raw.notch_filter(freqs=np.arange(50, 251, 50))
+            print('Notch filter done')
+        return self.raw
+
+    def _common_average_reference(self):
+        if self.raw is not None:
+            self.raw_car, _ = mne.set_eeg_reference(self.raw.copy(), 'average')
+            print('CAR done')
+        return self.raw_car
+
+    def extract_events(self, plot=True):
+        events, events_id = self._read_events()
+        if plot:
+            self._plot_events()
+        return events, events_id
+
+    def _read_events(self):
+        if self.raw is not None:
+            if self.task == 'film':
+                custom_mapping = {'Stimulus/music': 2,
+                                  'Stimulus/speech': 1,
+                                  'Stimulus/end task': 5}
+            elif self.task == 'rest':
+                custom_mapping = {'Stimulus/start task': 1,
+                                  'Stimulus/end task': 2}
+            else:
+                raise NotImplementedError
+            self.events, self.event_id = mne.events_from_annotations(self.raw, event_id=custom_mapping,
+                                                                     use_rounding=False)
+            print('Reading events done')
+        return self.events, self.event_id
+
+    def _plot_events(self):
+        if self.raw_car is not None:
+            self.raw_car.plot(events=self.events,
+                              start=0,
+                              duration=180,
+                              color='gray',
+                              event_color={2: 'g', 1: 'r'},
+                              bgcolor='w')
+
+    def extract_bands(self, smooth=False):
+        if self.raw_car is not None:
+            bands2 = self._compute_band_envelopes()
+            bands3 = self._crop_band_envelopes()
+            bands4 = self._resample_band_envelopes()
+            if smooth: self._smooth_band_envelopes()
+            bands5, bands6, band_block_means = self._compute_block_means_per_band()
+        return bands2, bands3, bands4, bands5
+
+    def _compute_band_envelopes(self):
+        if self.raw_car is not None:
+            bands = {'delta': [1, 4], 'theta': [5, 8], 'alpha': [8, 12], 'beta': [13, 24], 'gamma': [60, 120]}
+            self.bands1 = OrderedDict.fromkeys(bands.keys())
+            for key in self.bands1.keys():
+                self.bands1[key] = self.raw_car.copy().filter(bands[key][0], bands[key][1]).apply_hilbert(
+                    envelope=True).get_data().T
+            print('Extracting band envelopes done')
+        return self.bands1
+
+    def _crop_band_envelopes(self):
+        if self.bands1 is not None:
+            self.bands2 = OrderedDict.fromkeys(self.bands1.keys())
+            for key in self.bands1.keys():
+                self.bands2[key] = self.bands1[key][self.events[0, 0]:self.events[-1, 0]]
+        return self.bands2
+
+    def _resample_band_envelopes(self):
+        if self.bands1 is not None:
+            self.bands3 = OrderedDict.fromkeys(self.bands1.keys())
+            for key in self.bands1.keys():
+                self.bands3[key] = resample(self.bands2[key], 25, int(self.raw.info['sfreq']))
+        return self.bands3
+
+    def _smooth_band_envelopes(self):
+        if self.bands1 is not None:
+            for key in self.bands1.keys():
+                self.bands[key] = np.apply_along_axis(smooth_signal, 0, self.bands[key], 5)
+
+    def _compute_block_means_per_band(self):
+        if self.bands1 is not None:
+            self.band_block_means = OrderedDict.fromkeys(self.bands1.keys())
+            band6 = OrderedDict.fromkeys(self.bands1.keys())
+            for key in self.bands1.keys():
+                band6[key] = zscore(self.bands3[key][:self.expected_duration * 25])
+                band7 = band6[key].reshape((-1, 750, band6[key].shape[-1]))  # 13 blocks in chill or 6 in rest
+                self.band_block_means[key] = np.mean(band7, 1)
+        return band6, band7, self.band_block_means
--- main.py
+++ main.py
@@ -1,31 +1,94 @@
-import mne
-import matplotlib.pyplot as plt
+import mne_bids
+import os
 import numpy as np
-
-# add a coment
-
-header_file = 'F:/Datasets/ieeg_visual/ds003688-download/sub-03/ses-iemu/ieeg/sub-03_ses-iemu_task-film_acq' \
-              '-clinical_run-1_ieeg.vhdr '
-marker_file ='F:/Datasets/ieeg_visual/ds003688-download/sub-03/ses-iemu/ieeg/sub-03_ses-iemu_task-film_acq-clinical_run-1_ieeg.vmrk'
-
-filename = 'F:/Datasets/ieeg_visual/ds003688-download/sub-03/ses-iemu/ieeg/sub-03_ses-iemu_task-film_acq-clinical_run' \
-           '-1_ieeg.eeg '
-
-# Load the data using MNE-Python's io module
-raw = mne.io.read_raw_brainvision(header_file, preload=True, verbose=False)
-
-# Load the events from the marker file
-events = mne.events_from_annotations(raw)
-data = raw.get_data()
-# Print some information about the loaded data
-print(raw.info)
-
-fs = raw.info['sfreq']
-time = np.arange(0, data.shape[1])/(fs*60)
-plt.figure()
-plt.plot(time[:1000], data[33,:1000])
-plt.show()
-# Print some information about the events
-print(events[0][:10])
+from import_data import SubjectDataset
 
 
+# set device
+device = 'Navid'
+if device.lower() == 'navid':
+    data_path = 'F:/Datasets/ieeg_visual/ds003688-download/'
+elif device.lower() == 'maryam':
+    data_path = 'E:/maryam.sh'
+else:
+    data_path = ''
+
+# settings
+settings = {
+    'number_of_patients': 3,
+}
+
+subjects = mne_bids.get_entity_vals(data_path, 'subject')
+subject = subjects[0]
+acqs = mne_bids.get_entity_vals(os.path.join(data_path, 'sub-' + subject, 'ses-iemu', 'ieeg'), 'acquisition')
+acq = acqs[0]
+
+# Load all data
+band_all_patient = []
+raw_car_all = []
+subject_list = subjects[:settings['number_of_patients']]
+i = 0
+for subject in subject_list:
+    if 'iemu' in mne_bids.get_entity_vals(os.path.join(data_path, 'sub-' + subject), 'session'):
+        i = i + 1
+        s = SubjectDataset(data_path, subject, acquisition=acq)
+        raw_car = s.preprocess()
+        events, events_id = s.extract_events()
+        bands2, bands3, bands4, band5 = s.extract_bands()
+        band_all_patient.append(band5['gamma'])
+        raw_car_all.append(raw_car)
+
+
+# find common electrodes
+elec_more_one = []
+elec_more_ten = []
+elec_more_fifteen = []
+elec_more_tweny = []
+elec_more_thirty = []
+
+for key in h.keys():
+    if (h[key] > 1):
+        elec_more_one.append(key)
+    if (h[key] > 10):
+        elec_more_ten.append(key)
+    if (h[key] > 15):
+        elec_more_fifteen.append(key)
+    if (h[key] > 20):
+        elec_more_tweny.append(key)
+
+print('number of total electrode is =', len(h), '\nmax number of electrod is same=', 23)
+
+print('number of electrod that are same in more than one patient = ', len(elec_more_one))
+print('number of electrod that are same in more than ten patient = ', len(elec_more_ten))
+print('number of electrod that are same in more than fifteen patient = ', len(elec_more_fifteen))
+print('number of electrod that are same in more than tweny patient = ', len(elec_more_tweny))
+
+print('\n\n', elec_more_fifteen, 'elec_more_fifteen\n\n')
+
+print(elec_more_tweny, 'elec_more_tweny')
+
+
+elec= {'F01':[] , 'F03':[] , 'F05': [] , 'F06':[] , 'F07':[] , 'F08':[] , 'T01':[] , 'T02':[] , 'T03':[] , 'T04':[] , 'T05':[] , 'T06':[], 'T07':[], 'T09':[], 'T10':[], 'T11':[], 'T12':[], 'T13':[], 'T14':[], 'T08':[], 'T15':[]}
+
+for i in range(len(raw_car_all)):
+    for key in elec.keys():
+        if key in raw_car_all[i].ch_names:
+            elec[key].append(i)
+
+for key in elec.keys():
+    print('\n  ' ,key, '=', elec[key])
+
+
+plot_elec_avg= {'F01':0 , 'F03':0 , 'F05': 0 , 'F06':0 , 'F07':0 , 'F08':0 , 'T01':0 , 'T02':0 , 'T03':0 , 'T04':0 , 'T05':0 , 'T06':0, 'T07':0, 'T09':0, 'T10':0, 'T11':0, 'T12':0 , 'T13':0 , 'T14':0, 'T08':0, 'T15':0}
+time=np.arange(0, band_all_patient[0].shape[0])/(25)
+
+
+for key in plot_elec_avg.keys():
+    for i in elec[key]:
+        num_electrode=raw_car_all[i].ch_names.index(key)
+        plot_elec_avg[key]=plot_elec_avg[key]+band_all_patient[i][:,num_electrode]
+    plot_elec_avg[key]=plot_elec_avg[key]/len(elec[key])
+    plt.figure(figsize=(30,10))
+    plt.plot(time[:120*25],plot_elec_avg[key][:120*25])
+    plt.title(str(key),fontsize=25)
+    plt.show()
